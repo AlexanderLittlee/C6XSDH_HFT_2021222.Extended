a042262c39f03fa7bbe39f863242598e9e887b7a
sandorkiss@stud.uni-obuda.hu <sandorkiss@stud.uni-obuda.hu>
2022-11-16 20:36:40 +0100

    SignalR injected into the neccessary controller classes

--- C6XSDH_HFT_2021222.Endpoint/Controllers/BrandController.cs
+++ C6XSDH_HFT_2021222.Endpoint/Controllers/BrandController.cs
@@ -1,7 +1,9 @@
-﻿using C6XSDH_HFT_2021222.Logic.Interfaces;
+﻿using C6XSDH_HFT_2021222.Endpoint.Services;
+using C6XSDH_HFT_2021222.Logic.Interfaces;
 using C6XSDH_HFT_2021222.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +16,12 @@ namespace C6XSDH_HFT_2021222.Endpoint.Controllers
     public class BrandController : ControllerBase
     {
         ILogic<Brand> logic;
+        IHubContext<SignalRHub> hub;
 
-        public BrandController(ILogic<Brand> l)
+        public BrandController(ILogic<Brand> l, IHubContext<SignalRHub> hub)
         {
             logic = l;
+            this.hub = hub;
         }
 
 
@@ -34,21 +38,25 @@ namespace C6XSDH_HFT_2021222.Endpoint.Controllers
         }
 
         [HttpPost]
-        public void Create([FromBody] Brand b)
+        public void Create([FromBody] Brand b, IHubContext<SignalRHub> hub)
         {
             logic.Create(b);
+            this.hub.Clients.All.SendAsync("BrandCreated", b);
         }
 
         [HttpPut]
         public void Put([FromBody] Brand b)
         {
             logic.Update(b);
+            this.hub.Clients.All.SendAsync("BrandUpdated", b);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var b = logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("BrandDeleted", b);
         }
     }
 }
--- C6XSDH_HFT_2021222.Endpoint/Controllers/ScooterController.cs
+++ C6XSDH_HFT_2021222.Endpoint/Controllers/ScooterController.cs
@@ -1,7 +1,9 @@
-﻿using C6XSDH_HFT_2021222.Logic.Interfaces;
+﻿using C6XSDH_HFT_2021222.Endpoint.Services;
+using C6XSDH_HFT_2021222.Logic.Interfaces;
 using C6XSDH_HFT_2021222.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +16,12 @@ namespace C6XSDH_HFT_2021222.Endpoint.Controllers
     public class ScooterController : ControllerBase
     {
         IScooterLogic logic;
+        IHubContext<SignalRHub> hub;
 
         public ScooterController(IScooterLogic l)
         {
             logic = l;
+            this.hub = hub;
         }
 
 
@@ -37,18 +41,22 @@ namespace C6XSDH_HFT_2021222.Endpoint.Controllers
         public void Create([FromBody] Scooter s)
         {
             logic.Create(s);
+            this.hub.Clients.All.SendAsync("ScooterCreated", s);
         }
 
         [HttpPut]
         public void Put([FromBody] Scooter s)
         {
             logic.Update(s);
+            this.hub.Clients.All.SendAsync("ScooterUpdated", s);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var s= logic.Read(id);  
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("ScooterDeleted", s);
         }
     }
 }
