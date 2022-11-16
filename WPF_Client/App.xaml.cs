9f1ee33d1d4aabbfdbda93cdeef79dd3296863a0
sandorkiss@stud.uni-obuda.hu <sandorkiss@stud.uni-obuda.hu>
2022-11-16 17:27:46 +0100

    ViewModel walking skeleton made

--- WPF_Client/App.xaml.cs
+++ WPF_Client/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WPF_Client
+namespace C6XSDH_HFT_2021222.WPFClient
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- WPF_Client/MainWindowViewModel.cs
+++ WPF_Client/MainWindowViewModel.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using C6XSDH_HFT_2021222.Models.Entities;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WPF_Client
+
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ObservableRecipient
     {
+        private Bike selectedbike;
+
+        public Bike SelectedBike
+        {
+            get { return selectedbike; }
+            set { SetProperty(ref selectedbike, value);
+                (DeleteBike as RelayCommand).NotifyCanExecuteChanged();
+                (UpdateBike as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+
+
+        public RestCollection<Bike> Bikes { get; set; }
+        public RestCollection<Scooter> Scooters{ get; set; }
+        public RestCollection<Brand> Brands { get; set; }
+        public RestCollection<Purchase> Purchases{ get; set; }
+
+        public ICommand CreateBike { get; set; }
+        public ICommand UpdateBike { get; set; }
+        public ICommand DeleteBike { get; set; }
+        
+
+        public MainWindowViewModel()
+        {
+            Bikes = new RestCollection<Bike>("http://localhost:30408/","bike");
+            Scooters= new RestCollection<Scooter>("http://localhost:30408/", "scooter");
+            Brands = new RestCollection<Brand>("http://localhost:30408/", "brand");
+            Purchases = new RestCollection<Purchase>("http://localhost:30408/", "purchase");
+
+
+            CreateBike = new RelayCommand(() => { });
+            UpdateBike = new RelayCommand(() => {  }, () => { return SelectedBike != null; });
+            DeleteBike = new RelayCommand(() => { Bikes.Delete(SelectedBike.Id); }, () => { return SelectedBike != null; });
+        }
+
     }
 }
