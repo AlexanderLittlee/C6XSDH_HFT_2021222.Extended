5f714088cf8882ae0041b941fe62a25d308bea52
sandorkiss@stud.uni-obuda.hu <sandorkiss@stud.uni-obuda.hu>
2022-11-17 21:33:07 +0100

    Bike Catalog walking skeleton is walking and is skeleton.

--- C6XSDH_HFT_2021222.JSClient/wwwroot/script.js
+++ C6XSDH_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,3 +1,19 @@
-﻿fetch('http://localhost:30408/bike')
+﻿let bikes = [];
+
+fetch('http://localhost:30408/bike')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        bikes = y;
+        console.log(bikes)
+        display();
+    });
+
+function display() {
+    bikes.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>" + t.model + "</td><td>"
+            + t.price + "</td><td>" + t.rating + "</td></tr>";
+
+        console.log(t.id)
+    });
+}
